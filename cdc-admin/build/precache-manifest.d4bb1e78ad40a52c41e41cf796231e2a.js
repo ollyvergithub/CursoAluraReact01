fb55a4abe50dcbbee3fbf7fcd7b949b8718ce2ac
Ollyver Ottoboni <ollyver.ottoboni@amcom.com.br>
2019-08-20 08:46:36 -0300

    FINAL - Aula 08_01 com Build

---
+++ cdc-admin/build/precache-manifest.d4bb1e78ad40a52c41e41cf796231e2a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db87b1135bdfe60ebc0a68255fb5e6e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25579ef9f7af886f1527",
+    "url": "/static/css/main.659808ea.chunk.css"
+  },
+  {
+    "revision": "d6beb744d89a58114fb8",
+    "url": "/static/js/2.697b3a9c.chunk.js"
+  },
+  {
+    "revision": "25579ef9f7af886f1527",
+    "url": "/static/js/main.a18ab797.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
